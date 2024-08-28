7832f0877e3653da6749688f2d3f143e534d3bd2
ywzb <yuwenzhanbai@gmail.com>
2024-08-28 23:09:34 +0800

    Site updated: 2024-08-28 23:09:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/28/这算是开始/"];function toRandomPost(){
+var posts=["2024/08/28/这算是开始/","2024/08/28/开学前总是喜欢躺着/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
